97bed549b3a91c17e2f102589bb55c36cb003490
pushpa <pb02487@gmail.com>
2026-05-09 15:03:39 +0530

    sign up

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,13 +1,28 @@
 package com.scm.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.scm.entities.User;
+import com.scm.forms.UserForm;
+import com.scm.helpers.Message;
+import com.scm.helpers.MessageType;
+import com.scm.services.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
 public class PageController {
+
+    @Autowired
+    private UserService userService;
+    
     @RequestMapping("/home")
     public String home(Model model){
         System.out.println("Home Page handler");
@@ -49,9 +64,52 @@ public class PageController {
 
     // register
     @GetMapping("/register")
-    public String register() {
+    public String register(Model model) {
+        UserForm userForm = new UserForm();
+        //default data to data
+        // userForm.setName("Pushpa");
+        // userForm.setAbout("hii hello");
+        model.addAttribute("userForm",userForm);
         return "register";
     }
     
-    
+    //processing register
+    @RequestMapping(value="/do-register", method=RequestMethod.POST)
+    public String processRegister(@ModelAttribute UserForm userForm, HttpSession session){
+        System.out.println("Processing registration");
+        //fetch form data
+        //UserForm
+        System.out.println(userForm);
+        //validate form data
+        //save to database
+
+        //userservice
+        //userForm --> user
+        // User user = User.builder()
+        // .name(userForm.getName())
+        // .email(userForm.getEmail())
+        // .password(userForm.getPassword())
+        // .about(userForm.getAbout())
+        // .phoneNumber(userForm.getPhoneNumber())
+        // .profilePic("\"C:\\Users\\Pushpa\\Desktop\\avatar-gender-neutral-silhouette-vector-600nw-2470054311.webp\"")
+        // .build();
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        user.setAbout(userForm.getAbout());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        user.setProfilePic("\"C:\\Users\\Pushpa\\Desktop\\avatar-gender-neutral-silhouette-vector-600nw-2470054311.webp\"");
+        User savedUser = userService.saveUser(user);
+        System.out.println("User saved: ");
+
+        //messaage = 'Registration successful'
+
+        //add the message
+        Message message = Message.builder().content("Registration Successful").type(MessageType.blue).build();
+        session.setAttribute("message",message);
+
+        //redirect to login page
+        return "redirect:/register";
+    }
 }
